2fbace397b005727e44c963a6558d7b9722aa51e
mathewsmoreira <mathewsoliveira29@gmail.com>
2024-09-20 18:55:19 -0300

    feat: inicialização do projeto com spring boot.

---
+++ src/main/java/org/devlouco/bacensenderhub/BacenSenderHubApplication.java
@@ -0,0 +1,13 @@
+package org.devlouco.bacensenderhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacenSenderHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BacenSenderHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/devlouco/bacensenderhub/BacenSenderHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devlouco.bacensenderhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacenSenderHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
