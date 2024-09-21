582a923376fd21315b871aca53ec7a1fc816fb79
mathewsmoreira <mathewsoliveira29@gmail.com>
2024-09-20 22:13:16 -0300

    feat(companyModel): Adicionado classe model
    Criação do companyModel para representar os dados cadastrais de uma compania.

---
+++ src/main/java/org/devlouco/bacensenderhub/models/CompanyModel.java
@@ -0,0 +1,42 @@
+package org.devlouco.bacensenderhub.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class CompanyModel {
+
+    public CompanyModel(int coop, String cnpj) {
+        this.coop = coop;
+        this.cnpj = cnpj;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long ID;
+
+
+    @Min(1)
+    @Max(4)
+    @Column(unique = true, nullable = false, length = 4)
+    private int coop;
+
+    @NotBlank
+    @Size(max = 14)
+    @Column(unique = true, nullable = false)
+    private String cnpj;
+
+    @OneToOne(mappedBy = "company", cascade = CascadeType.ALL)
+    private CredentialsModel credentials;
+
+
+
+}
