46ce61c17185d1b5f23ff2f7895390665f396bdc
mathewsmoreira <mathewsoliveira29@gmail.com>
2024-09-22 10:14:29 -0300

    feat(protocolModel): Adicionado classe model
    Criação do ProtocolModel para representar os dados de protocolo de uma requisição post para a webservice do STA, end point POST /staws/arquivos.

---
+++ src/main/java/org/devlouco/bacensenderhub/models/ProtocolModel.java
@@ -0,0 +1,187 @@
+package org.devlouco.bacensenderhub.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+import javax.xml.bind.annotation.*;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Objects;
+
+@Entity
+@XmlRootElement(name = "Parametros")
+@XmlType(propOrder = {"idetification","hash","size","docName","observation","recipient"})
+public class ProtocolModel {
+
+    public ProtocolModel(Long ID, LocalDate date, String idetification, String hash, long size, String docName, String observation, String recipient, CompanyModel company, byte[] filesBytes) {
+        this.ID = ID;
+        this.date = date;
+        this.idetification = idetification;
+        this.hash = hash;
+        this.size = size;
+        this.docName = docName;
+        this.observation = observation;
+        this.recipient = recipient;
+        this.company = company;
+        this.filesBytes = filesBytes;
+    }
+
+    public ProtocolModel() {}
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+
+    @NotNull
+    private LocalDate date;
+
+
+    @NotBlank
+    private String idetification;
+
+    @NotBlank
+    @Size(max = 64)
+    private String hash;
+
+
+    private long size;
+
+    @NotBlank
+    private String docName;
+
+    private String observation;
+
+    @NotBlank
+
+    private String recipient;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private CompanyModel company;
+
+    private byte filesBytes[];
+
+
+    @XmlTransient
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    @XmlTransient
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @XmlElement(name = "IdentificadorDocumento")
+    public String getIdetification() {
+        return idetification;
+    }
+
+    public void setIdetification(String idetification) {
+        this.idetification = idetification;
+    }
+
+    @XmlElement(name = "Hash")
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    @XmlElement(name = "Tamanho")
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+    @XmlElement(name = "NomeArquivo")
+    public String getDocName() {
+        return docName;
+    }
+
+    public void setDocName(String docName) {
+        this.docName = docName;
+    }
+
+    @XmlElement(name = "Observacao")
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+
+    @XmlElement(name = "Destinatarios")
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    @XmlTransient
+    public CompanyModel getCompany() {
+        return company;
+    }
+
+    public void setCompany(CompanyModel company) {
+        this.company = company;
+    }
+
+    @XmlTransient
+    public byte[] getFilesBytes() {
+        return filesBytes;
+    }
+
+    public void setFilesBytes(byte[] filesBytes) {
+        this.filesBytes = filesBytes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ProtocolModel that)) return false;
+        return Objects.equals(ID, that.ID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(ID);
+    }
+
+    @Override
+    public String toString() {
+        return "ProtocolModel{" +
+                "ID=" + ID +
+                ", date=" + date +
+                ", idetification='" + idetification + '\'' +
+                ", hash='" + hash + '\'' +
+                ", size=" + size +
+                ", docName='" + docName + '\'' +
+                ", observation='" + observation + '\'' +
+                ", recipient='" + recipient + '\'' +
+                ", company=" + company +
+                ", filesBytes=" + Arrays.toString(filesBytes) +
+                '}';
+    }
+}
