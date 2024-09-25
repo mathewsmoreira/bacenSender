d9a17f0fa9ccf2ae17e02ac772cc0457981fe695
mathewsmoreira <mathewsoliveira29@gmail.com>
2024-09-25 09:01:01 -0300

    feat(companyModelRepository): Adicionado classe repository
    Criação do CompanyModelRepository para efetuar as manipulações com o banco de dados da classe CompanyModel.

---
+++ src/main/java/org/devlouco/bacensenderhub/repositories/CompanyModelRepository.java
@@ -0,0 +1,7 @@
+package org.devlouco.bacensenderhub.repositories;
+
+import org.devlouco.bacensenderhub.models.CompanyModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyModelRepository extends JpaRepository<CompanyModel, Long> {
+}
