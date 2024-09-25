f793504a8070ba036bbc21f6f2154d150436ee4b
mathewsmoreira <mathewsoliveira29@gmail.com>
2024-09-25 09:17:17 -0300

    feat(credentialsModelRepository): Adicionado classe repository
    Criação do CredentialsModelRepository para efetuar as manipulações com o banco de dados da classe CredentialsModel.

---
+++ src/main/java/org/devlouco/bacensenderhub/repositories/CredentialsModelRepository.java
@@ -0,0 +1,7 @@
+package org.devlouco.bacensenderhub.repositories;
+
+import org.devlouco.bacensenderhub.models.CredentialsModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CredentialsModelRepository extends JpaRepository<CredentialsModel, Long> {
+}
