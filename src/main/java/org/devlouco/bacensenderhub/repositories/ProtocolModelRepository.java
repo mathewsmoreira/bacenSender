966fdbfb67fca376e397c046e87a49ff5ac399c3
mathewsoliveira29@gmail.com <MatheusM.Oliveira@sicoob.com.br>
2024-09-26 17:09:59 -0300

    fix(protocolModelRepository): Correcao de Bug classe repository
    Modificado o JpaRepository<ProtocolModel, Integer> por JpaRepository<ProtocolModel, Long>, para igualar com a chave primaria do ProtocolModel.

--- src/main/java/org/devlouco/bacensenderhub/repositories/ProtocolModelRepository.java
+++ src/main/java/org/devlouco/bacensenderhub/repositories/ProtocolModelRepository.java
@@ -1,6 +1,7 @@
 package org.devlouco.bacensenderhub.repositories;
 
+import org.devlouco.bacensenderhub.models.ProtocolModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProtocolModelRepository extends JpaRepository<ProtocolModelRepository, Integer> {
+public interface ProtocolModelRepository extends JpaRepository<ProtocolModel, Long> {
 }
