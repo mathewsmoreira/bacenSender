f9229880c461aab15ac3815b112b7ff55632e70a
mathewsoliveira29@gmail.com <MatheusM.Oliveira@sicoob.com.br>
2024-09-27 14:44:05 -0300

    test(protocolModelRepositoryService): Adicionado metodos a classe test
    Adição do metodos para teste de todo o CRUD do ProtocolModelRepositoryServiceTest.

--- src/test/java/org/devlouco/bacensenderhub/services/ProtocolModelRepositoryServiceTest.java
+++ src/test/java/org/devlouco/bacensenderhub/services/ProtocolModelRepositoryServiceTest.java
@@ -1,17 +1,26 @@
 package org.devlouco.bacensenderhub.services;
 
 import org.devlouco.bacensenderhub.models.CompanyModel;
+import org.devlouco.bacensenderhub.models.CredentialsModel;
 import org.devlouco.bacensenderhub.models.ProtocolModel;
 import org.devlouco.bacensenderhub.repositories.ProtocolModelRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.parameters.P;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 class ProtocolModelRepositoryServiceTest {
@@ -26,7 +35,6 @@ class ProtocolModelRepositoryServiceTest {
 
     private ProtocolModel protocolModel;
 
-    @Mock
     private byte[] fileByte;
 
     @BeforeEach
@@ -38,6 +46,76 @@ class ProtocolModelRepositoryServiceTest {
         protocolModel = new ProtocolModel(
                 1l, LocalDate.now(), "4010","9179ec02b930c1bd44744d784680b21f6ebcb4c1fccbfe1a5bc0f64df7559d58",
                 256,"terste.zip","","544",companyModel,fileByte);
+    }
+
+    @Test
+    void Dado_uma_entidade_valida_Quando_salvar_Entao_retorna_uma_entidade(){
+        when(repo.save(any())).thenReturn(
+                protocolModel
+        );
+
+        ProtocolModel save = service.save(protocolModel);
+        assertAll(
+                ()->{
+                    assertNotNull(save);
+                    assertEquals(protocolModel.getHash(),save.getHash());
+                    verify(repo, times(1)).save(any());
+                }
+        );
+    }
+
+    @Test
+    void Dado_uma_entidade_valida_Quando_tentar_salvar_nulo_Entao_deve_lancar_exception(){
+        assertThrows(NullPointerException.class, () ->service.save(null));
+
+    }
+
+    @Test
+    void Dado_uma_entidade_valida_Quando_feito_o_update_Entao_deve_retornar_a_entidade(){
+        when(repo.save(any())).thenReturn(
+                protocolModel
+        );
+
+        ProtocolModel update = service.update(protocolModel);
+        assertAll(
+                ()->{
+                    assertNotNull(update);
+                    assertEquals(protocolModel.getHash(), update.getHash());
+                    verify(repo,times(1)).save(any());
+                }
+        );
+    }
+
+    @Test
+    void Dado_uma_entidade_valida_Quando_feito_o_findProtocolbyId_Entao_deve_retornar_a_entidade(){
+        when(repo.findById(anyLong())).thenReturn(
+                Optional.of(protocolModel)
+        );
+
+        ProtocolModel update = service.findProtocolbyId(protocolModel);
+        assertAll(
+                ()->{
+                    assertNotNull(update);
+                    assertEquals(1l,update.getID());
+                    verify(repo,times(1)).findById(anyLong());
+                }
+        );
+    }
+
+    @Test
+    void Dado_uma_entidade_valida_Quando_chamado_findAll_protocolModel_Entao_retorna_uma_lista_de_entidade(){
+        when(repo.findAll()).thenReturn(
+                List.of(protocolModel)
+        );
+
+        List<ProtocolModel> all = service.findAllProtocol();
+        assertAll(
+                ()->{
+                    assertNotNull(all);
+                    assertEquals(1, all.size());
+                    verify(repo, times(1)).findAll();
+                }
+        );
     }
 
 
