cba42004a61773c6a35c7b2e85148c711c0085f8
mathewsmoreira <mathewsoliveira29@gmail.com>
2024-09-27 16:18:46 -0300

    refactor(genericCrudService): Refatoração classe service
    Refatoração do services para usar a abstração GenericCrudService, diminuindo codigo, e facilitando a manutenção futura.

--- src/main/java/org/devlouco/bacensenderhub/services/CredentialsModelRepositoryService.java
+++ src/main/java/org/devlouco/bacensenderhub/services/CredentialsModelRepositoryService.java
@@ -4,37 +4,15 @@ import org.devlouco.bacensenderhub.models.CredentialsModel;
 import org.devlouco.bacensenderhub.repositories.CredentialsModelRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Objects;
-
 @Service
-public class CredentialsModelRepositoryService {
-
-    private CredentialsModelRepository credentialsModelRepository;
+public class CredentialsModelRepositoryService extends GenericCrudService<CredentialsModel, Long> {
 
 
-    public CredentialsModel save(CredentialsModel credentialsModel) {
-        Objects.requireNonNull(credentialsModel);
-        return credentialsModelRepository.save(credentialsModel);
+    public CredentialsModelRepositoryService(CredentialsModelRepository credentialsModelRepository) {
+        super(credentialsModelRepository);
     }
 
-    public CredentialsModel update(CredentialsModel credentialsModel) {
-        Objects.requireNonNull(credentialsModel);
-        return credentialsModelRepository.save(credentialsModel);
-    }
 
-    public CredentialsModel findCredentialsById(CredentialsModel credentialsModel ) {
-        Objects.requireNonNull(credentialsModel);
-        return credentialsModelRepository.findById(credentialsModel.getID()).orElse(null);
-    }
-
-    public List<CredentialsModel> findAllCredentials() {
-        return credentialsModelRepository.findAll();
-    }
-
-    public void deleteCredentialsModel(CredentialsModel credentialsModel) {
-        credentialsModelRepository.delete(credentialsModel);
-    }
 
 
 }
--- src/main/java/org/devlouco/bacensenderhub/services/ProtocolModelRepositoryService.java
+++ src/main/java/org/devlouco/bacensenderhub/services/ProtocolModelRepositoryService.java
@@ -2,40 +2,13 @@ package org.devlouco.bacensenderhub.services;
 
 import org.devlouco.bacensenderhub.models.ProtocolModel;
 import org.devlouco.bacensenderhub.repositories.ProtocolModelRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Objects;
-
 @Service
-public class ProtocolModelRepositoryService {
+public class ProtocolModelRepositoryService extends GenericCrudService<ProtocolModel, Long> {
 
-    @Autowired
-    private ProtocolModelRepository repo;
-
-    public ProtocolModel save(ProtocolModel protocolModel){
-        Objects.requireNonNull(protocolModel);
-        return repo.save(protocolModel);
-    }
-
-    public ProtocolModel update(ProtocolModel protocolModel){
-        Objects.requireNonNull(protocolModel);
-        return repo.save(protocolModel);
-    }
-
-    public void delete(ProtocolModel protocolModel){
-        Objects.requireNonNull(protocolModel);
-        repo.delete(protocolModel);
-    }
-
-    public ProtocolModel findProtocolbyId(ProtocolModel protocolModel){
-        Objects.requireNonNull(protocolModel);
-        return repo.findById(protocolModel.getID()).orElse(null);
-    }
-
-    public List<ProtocolModel> findAllProtocol(){
-        return repo.findAll();
+    public ProtocolModelRepositoryService(ProtocolModelRepository protocolModelRepository) {
+        super(protocolModelRepository);
     }
 
 
