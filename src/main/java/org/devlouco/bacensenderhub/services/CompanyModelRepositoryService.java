d7d14848b0712a8c54cd0478004edda0723829ea
mathewsmoreira <mathewsoliveira29@gmail.com>
2024-10-02 08:23:10 -0300

    feat(companyModelRepository): Adicionado metodo a classe repository
    Criação do metodo para pesquisar uma company pelo atributo coop, e a annotation @Repository.

--- src/main/java/org/devlouco/bacensenderhub/services/CompanyModelRepositoryService.java
+++ src/main/java/org/devlouco/bacensenderhub/services/CompanyModelRepositoryService.java
@@ -7,7 +7,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class CompanyModelRepositoryService extends GenericCrudService<CompanyModel, Long> {
 
+    private CompanyModelRepository repository;
+
     public CompanyModelRepositoryService(CompanyModelRepository repository) {
         super(repository);
+    }
+
+    public CompanyModel getCompanyModelByCoop(int coop) {
+
+        return this.repository.findByCoop(coop).orElseThrow(
+                () -> new RuntimeException("Could not find company model with coop " + coop)
+        );
     }
 }
