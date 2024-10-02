dd0ef580987709d46bdb35929c49a7e089dd2ddf
mathewsmoreira <mathewsoliveira29@gmail.com>
2024-10-02 08:37:34 -0300

    feat(credentialsModeRepository): Adicionado metodo a classe repository
    Criação do metodo para pesquisar uma credentials pelo atributo coop, e a annotation @Repository.

--- src/main/java/org/devlouco/bacensenderhub/repositories/CredentialsModelRepository.java
+++ src/main/java/org/devlouco/bacensenderhub/repositories/CredentialsModelRepository.java
@@ -1,7 +1,17 @@
 package org.devlouco.bacensenderhub.repositories;
 
+import org.devlouco.bacensenderhub.models.CompanyModel;
 import org.devlouco.bacensenderhub.models.CredentialsModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface CredentialsModelRepository extends JpaRepository<CredentialsModel, Long> {
+
+    @Query("SELECT c FROM CredentialsModel c where c.company.coop= :coop")
+    public Optional<CredentialsModel> findByCoop(@Param("coop") int coop);
 }
